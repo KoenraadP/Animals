0812fed4ab445d8285fddef4eb5a5745fb20719d
Koenraad Pecceu <koenraad.pecceu@student.howest.be>
2024-04-28 14:17:52 +0200

    Add project files.

---
+++ Animals/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    internal class Dog : Animal
+    {
+        public string Breed { get; set; }
+
+        public Dog(int legs, bool canFly,
+            int averageLifeExpectancy, string breed) : base(legs,
+                canFly, averageLifeExpectancy)
+        {
+            Breed = breed;
+        }
+    }
+}
---
+++ Animals/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal a = new Animal(3,true,5);
+            Dog d = new Dog(4,false,60, "Duitse Herder");
+
+            Console.WriteLine(a);
+            Console.WriteLine(d);
+        }
+    }
+}

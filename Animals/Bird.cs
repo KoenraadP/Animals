a53790473b54da0c529fb99308c0036ebefd70e3
Koenraad Pecceu <koenraad.pecceu@creo.be>
2024-05-03 10:41:37 +0200

    Add Bird

---
+++ Animals/Bird.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    internal class Bird : Animal
+    {
+        public string[] Colors { get; set; }
+
+        public Bird(int legs, bool canFly, int avgLifeExpect, string[] colors) : base(legs, canFly, avgLifeExpect)
+        {
+            Colors = colors;
+        }
+
+        public string Talk()
+        {
+            return "Tweet! Tweet!";
+        }
+    }
+}
